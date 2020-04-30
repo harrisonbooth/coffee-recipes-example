5527db003e53101acb7798d6a980695d08fb7dde
Harrison Booth <harrisonboothh@gmail.com>
2020-04-30 11:34:07 +0100

    Add fetching recipes to coffeecontainer, setting the state

--- client/src/containers/CoffeeContainer.js
+++ client/src/containers/CoffeeContainer.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 
 class CoffeeContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recipes: []
+    };
+  }
+
+  componentDidMount() {
+    this.getRecipes()
+      .then(recipes => this.setState({ recipes: recipes }));
+  }
+
+  getRecipes() {
+    const url = 'http://localhost:3002/api/recipes';
+
+    return fetch(url)
+      .then(res => res.json());
+  }
 
   render() {
     return <h1>Coffee Recipes</h1>;
