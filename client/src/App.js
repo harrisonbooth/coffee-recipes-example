799bfaca0518254bdc02e889c2fdf0df3e268b50
Harrison Booth <harrisonboothh@gmail.com>
2020-04-30 11:29:03 +0100

    Add placeholder coffee container

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import CoffeeContainer from './containers/CoffeeContainer';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <CoffeeContainer />
   );
 }
 
---
+++ client/src/containers/CoffeeContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class CoffeeContainer extends Component {
+
+  render() {
+    return <h1>Coffee Recipes</h1>;
+  }
+}
+
+export default CoffeeContainer;
